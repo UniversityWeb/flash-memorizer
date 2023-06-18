facfab1cbd327ac3c2c9140bf16c18710d56cb2a
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2023-06-18 10:21:22 +0700

    setup docker to build full project

---
+++ src/main/java/com/universityteam/flashmemorizer/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.universityteam.flashmemorizer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String hello() {
+        return "home";
+    }
+}
