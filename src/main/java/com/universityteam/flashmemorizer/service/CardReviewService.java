2fdf01ccb73fe73d48f3015ae7606a7a7759cfaf
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2023-07-05 15:48:27 +0700

    add review card service

---
+++ src/main/java/com/universityteam/flashmemorizer/service/CardReviewService.java
@@ -0,0 +1,12 @@
+package com.universityteam.flashmemorizer.service;
+
+import com.universityteam.flashmemorizer.dto.CardReview;
+import com.universityteam.flashmemorizer.entity.Card;
+import com.universityteam.flashmemorizer.enums.EReview;
+
+import java.util.List;
+
+public interface CardReviewService {
+    List<CardReview> generateTest(EReview eReview, List<Card> cards);
+    String getHtmlFile(EReview eReview);
+}
---
+++ src/main/java/com/universityteam/flashmemorizer/service/impl/CardReviewServiceImpl.java
@@ -0,0 +1,30 @@
+package com.universityteam.flashmemorizer.service.impl;
+
+import com.universityteam.flashmemorizer.dto.CardReview;
+import com.universityteam.flashmemorizer.entity.Card;
+import com.universityteam.flashmemorizer.enums.EReview;
+import com.universityteam.flashmemorizer.service.CardReviewService;
+import com.universityteam.flashmemorizer.strategy.reviewcard.ReviewFactory;
+import com.universityteam.flashmemorizer.strategy.reviewcard.ReviewStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CardReviewServiceImpl implements CardReviewService {
+
+    @Autowired
+    private ReviewFactory reviewFactory;
+
+    public List<CardReview> generateTest(EReview eReview, List<Card> cards) {
+        ReviewStrategy strategy = reviewFactory.create(eReview);
+        return strategy.generateTest(cards);
+    }
+
+    @Override
+    public String getHtmlFile(EReview eReview) {
+        ReviewStrategy strategy = reviewFactory.create(eReview);
+        return strategy.getHtmlFile();
+    }
+}
