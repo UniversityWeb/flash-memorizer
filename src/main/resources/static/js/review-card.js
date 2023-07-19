0700c9366256fc2443a025289f2fb0f16e250fb8
Van Hoang <nbdev82@gmail.com>
2023-07-19 16:29:53 +0700

    move js script in review-card page into js file

---
+++ src/main/resources/static/js/review-card.js
@@ -0,0 +1,12 @@
+var carousel = document.getElementById('carouselExampleCaptions');
+carousel.addEventListener('slide.bs.carousel', function (event) {
+    var currentSlide = event.to +1;
+    var totalSlides = carousel.querySelectorAll('.carousel-item').length;
+    var progress = (currentSlide / totalSlides) * 100;
+    var progressBarIndicator = document.getElementById('progress-indicator');
+    var progressBar = document.getElementById('progress-bar');
+    var progressNumber = document.getElementById("progress-number");
+    progressBar.style.width = progress + '%';
+    progressBar.setAttribute('aria-valuenow', progress.toString());
+    progressNumber.innerHTML = currentSlide + '/' + totalSlides;
+});
