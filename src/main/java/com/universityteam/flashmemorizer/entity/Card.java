5e760cfb1c8db972193521b2471b5c6db3629a1e
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2023-07-31 16:11:35 +0700

    delete creation and modified field in card entity

--- src/main/java/com/universityteam/flashmemorizer/entity/Card.java
+++ src/main/java/com/universityteam/flashmemorizer/entity/Card.java
@@ -1,7 +1,10 @@
 package com.universityteam.flashmemorizer.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
