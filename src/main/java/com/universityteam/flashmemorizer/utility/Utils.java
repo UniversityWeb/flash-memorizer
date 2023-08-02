60b6eab46791224c32762eecf8b7cc8976d3147c
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2023-08-02 12:45:40 +0700

    convert html to plain text

--- src/main/java/com/universityteam/flashmemorizer/utility/Utils.java
+++ src/main/java/com/universityteam/flashmemorizer/utility/Utils.java
@@ -1,5 +1,9 @@
 package com.universityteam.flashmemorizer.utility;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,5 +13,23 @@ public class Utils {
         List<T> result = new ArrayList<>(a);
         result.removeAll(b);
         return result;
+    }
+
+    public static String htmlToPlainText(String html) {
+        Document document = Jsoup.parse(html);
+        return getPlainText(document.body());
+    }
+
+    private static String getPlainText(Element element) {
+        StringBuilder plainText = new StringBuilder();
+        for (Element child : element.children()) {
+            String childText = getPlainText(child);
+            plainText.append(childText);
+        }
+        String ownText = element.ownText();
+        if (!ownText.isEmpty()) {
+            plainText.append(ownText).append(" ");
+        }
+        return plainText.toString();
     }
 }
