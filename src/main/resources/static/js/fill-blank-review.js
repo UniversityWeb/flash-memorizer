7f203f419cbb4bc15512b462e7abde489e87b395
Nguyen Thi Thanh Tuyen <camuccon98@gmail.com>
2023-08-03 22:44:02 +0700

    add comments in fill blank and multichoice review

--- src/main/resources/static/js/fill-blank-review.js
+++ src/main/resources/static/js/fill-blank-review.js
@@ -1,12 +1,18 @@
+// Wait for the DOM content to load before executing the script
 document.addEventListener("DOMContentLoaded", function () {
+
     var nextButtons = document.querySelectorAll('.btn-next');
     nextButtons.forEach(function (nextButton) {
         nextButton.addEventListener("click", function () {
+
             var card = this.closest('.outer-card');
 
             if (card) {
+                // Calculate the height of the 'outer-card' element, including margins
                 var cardStyleHeight = window.getComputedStyle(card);
                 var cardHeight = card.offsetHeight + parseInt(cardStyleHeight.marginTop) + parseInt(cardStyleHeight.marginBottom);
+
+                // Scroll the window down by the height of the 'outer-card' element with smooth behavior
                 window.scrollBy({ top: cardHeight, behavior: 'smooth' });
             }
         });
@@ -20,15 +26,18 @@ document.addEventListener("DOMContentLoaded", function () {
             var cardInputValue = "";
 
             inputs.forEach(function (input) {
+                // Get the input with no extra spaces
                 var inputValue = input.value.trim();
-                            if (inputValue === "") {
-                                cardInputValue += "null-input-value ";
-                            } else {
-                                cardInputValue += inputValue + " ";
-                            }
+
+                if (inputValue === "") {
+                    cardInputValue += "null-input-value ";
+                } else {
+                    cardInputValue += inputValue + " ";
+                }
             });
 
             cardInputValue = cardInputValue.trim();
+            // Replace multiple spaces with a single space
             cardInputValue = cardInputValue.replace(/\s+/g, ' ');
 
             var hiddenInput = card.querySelector('.hidden-user-choices');
--- src/main/resources/static/js/multi-choice-review.js
+++ src/main/resources/static/js/multi-choice-review.js
@@ -1,21 +1,27 @@
 var options = document.querySelectorAll('input[type="radio"]');
-    options.forEach(function(option) {
-        option.addEventListener('click', function() {
-            var card = document.querySelector('.outer-card');
-            var cardStyleHeight = window.getComputedStyle(card);
-            var cardHeight = card.offsetHeight + parseInt(cardStyleHeight.marginTop) + parseInt(cardStyleHeight.marginBottom);
+options.forEach(function(option) {
+    // Attach a 'click' event listener to each radio button
+    option.addEventListener('click', function() {
+        var card = document.querySelector('.outer-card');
+        
+        // Calculate the height of the 'outer-card' element, including margins
+        var cardStyleHeight = window.getComputedStyle(card);
+        var cardHeight = card.offsetHeight + parseInt(cardStyleHeight.marginTop) + parseInt(cardStyleHeight.marginBottom);
 
-            window.scrollBy({ top: cardHeight, behavior: 'smooth' });
-        });
+        // Scroll the window down by the height of the 'outer-card' element with smooth behavior
+        window.scrollBy({ top: cardHeight, behavior: 'smooth' });
     });
+});
 
 const radioButtons = document.querySelectorAll('input[type="radio"]');
 radioButtons.forEach((radio) => {
+    // Attach a 'change' event listener to each radio button
     radio.addEventListener('change', (event) => {
         const selectedOptionText = event.target.nextElementSibling.textContent;
         const cardReviewIndex = event.target.getAttribute('data-card-review-index');
         const hiddenInput = document.getElementById('cardReviewIndexChoose' + cardReviewIndex);
 
+        // Update the value of the hidden input with the trimmed text content of the selected option
         hiddenInput.value = selectedOptionText.trim();
     });
 });
