10b9211a748de280195ab3155e15a47fdb450300
Van Hoang <nbdev82@gmail.com>
2023-08-08 17:21:53 +0700

    implement ImageService

--- src/main/java/com/universityteam/flashmemorizer/service/impl/ImageServiceImpl.java
+++ src/main/java/com/universityteam/flashmemorizer/service/impl/ImageServiceImpl.java
@@ -1,19 +1,38 @@
 package com.universityteam.flashmemorizer.service.impl;
 
+import com.universityteam.flashmemorizer.converter.DeckConverter;
+import com.universityteam.flashmemorizer.service.CardService;
 import com.universityteam.flashmemorizer.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
+@Service
 public class ImageServiceImpl implements ImageService {
     @Override
     public byte[] convertPathToByte(String path) {
+        byte[] imageBytes = null;
         try {
-            byte[] imageData = Files.readAllBytes(Paths.get(path));
-            return imageData;
+            // Read the image file into a BufferedImage
+            File imageFile = new File(path);
+            BufferedImage image = ImageIO.read(imageFile);
+
+            // Check if the image was successfully read
+            if (image != null) {
+                int width = image.getWidth();
+                int height = image.getHeight();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ImageIO.write(image, "jpg", baos);
+                imageBytes = baos.toByteArray();
+            } else {
+            }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
+        return imageBytes;
     }
 }
