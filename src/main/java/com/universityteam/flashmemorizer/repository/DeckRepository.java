3be3533598f991b23a64f5a18b3a2844fec79d9d
Anh Khoa Mark <qazblp2773652@gmail.com>
2023-08-13 22:54:08 +0700

    completed finding decks by username

--- src/main/java/com/universityteam/flashmemorizer/repository/DeckRepository.java
+++ src/main/java/com/universityteam/flashmemorizer/repository/DeckRepository.java
@@ -1,7 +1,9 @@
 package com.universityteam.flashmemorizer.repository;
 
 import com.universityteam.flashmemorizer.entity.Deck;
+import com.universityteam.flashmemorizer.entity.SharedDeck;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,9 @@ import java.util.List;
 public interface DeckRepository extends JpaRepository<Deck, Long> {
     Long countById(Long id);
     List<Deck> findByUserId(Long userId);
+
+    @Query(value = "SELECT d.* FROM decks d " +
+            "INNER JOIN users u ON d.user_id = u.id " +
+            "WHERE u.username = :username", nativeQuery = true)
+    List<Deck> findByUsername(String username);
 }
